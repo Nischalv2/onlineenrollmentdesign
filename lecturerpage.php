ce45e4a020e58f6261094f6ffd2c3f7a69d32ffc
unknown <Aspire2 Student@pc2.Nischal.com>
2022-09-05 11:28:39 +1200

    fcommit

--- lecturerpage.php
+++ lecturerpage.php
@@ -1,18 +1,5 @@
 <?php
-/*$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "online_enrollment_design";
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error)
-die("Connection failed: " . $conn->connect_error);
-else
-// echo "Successfully Connection to the database!";*/
-
-$servername = "us-cdbr-east-06.cleardb.net";
-$username = "b8088fe756207e";
-$password = "248eb91b";
-$dbname = "heroku_f75c68dec6849f1";
+include 'connection.php';
 
 if(isset($_POST['search'])){//check search value is set or not
     $sql = "SELECT * FROM lecturer WHERE LID = ? OR Name = ? OR Lastname = ?" ;
